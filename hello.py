b7a57d767e6f6c9f4163600e9e3b9d46757de396
wildmental <pbjworking@gmail.com>
2017-01-15 22:01:20 +0900

    Github work goes pulled without fetching

--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ print("hello testbranch!")
 print("remote repository may have some change...! but, I'm changed too...!")
 //Github works
 print("this line makes local cannot push it's work here easily")
+print("pull this line!")
